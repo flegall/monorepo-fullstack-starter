f9105ade30d050eacbcbf5d31a5664313e5e1254
Florent Le Gall <florent.legall@gmail.com>
2017-01-20 11:52:00 +0100

    Add node6 babel preset

--- server/webpack/webpack.config.js
+++ server/webpack/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         query: {
-                            presets: ['es2015']
+                            presets: ['node6']
                         },
                     }
                 ]
