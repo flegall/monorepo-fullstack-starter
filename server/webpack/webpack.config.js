20511750bb4b03e35dd20b49393e35232f29a632
Florent Le Gall <florent.legall@gmail.com>
2017-01-21 11:05:06 +0100

    Add jest-haste-map

--- server/webpack/webpack.config.js
+++ server/webpack/webpack.config.js
@@ -1,6 +1,21 @@
 var path = require('path');
 var webpack = require('webpack');
 var ProgressBarPlugin = require('progress-bar-webpack-plugin');
+var HasteMap = require('jest-haste-map');
+const _ = require('lodash');
+
+new HasteMap({
+    extensions: ['js', 'json'],
+    ignorePattern: /VERYCOMPLEX_IGNORE_PATTERN/,
+    roots: [path.resolve(__dirname, '../..')],
+    maxWorkers: 1,
+    name: 'haste-map-test',
+    platforms: ['ios', 'android'],
+    resetCache: false,
+    useWatchman: true,
+}).build().then((result) => {
+    // console.log(JSON.stringify(result.moduleMap, null, 2));
+});
 
 module.exports = {
     context: path.resolve(__dirname, '..'),
