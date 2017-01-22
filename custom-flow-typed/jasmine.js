f7b20d276cadce5c9d0d0881bee1102373d9eb34
Florent Le Gall <florent.legall@gmail.com>
2017-01-22 13:58:50 +0100

    Add flow

---
+++ custom-flow-typed/jasmine.js
@@ -0,0 +1,7 @@
+declare function describe(suiteMessage: string, suite:() => void): void;
+declare function xdescribe(suiteMessage: string, suite:() => void): void;
+declare function it(testMessage: string, test:() => void | Promise<void>): void;
+declare function pit(testMessage: string, test:() => Promise<void>): void;
+declare function xit(testMessage: string, test:() => void | Promise<void>): void;
+declare function beforeEach(callback:(done: () => void) => any): void;
+declare function afterEach(callback:() => void): void;
