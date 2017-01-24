fcd31708cf94c571f792a447fd4b5d306bc0c622
Florent Le Gall <florent.legall@gmail.com>
2017-01-24 14:58:37 +0100

    Add haste map webpack resolver

--- server/webpack/webpack.config.js
+++ server/webpack/webpack.config.js
@@ -1,54 +1,9 @@
+const _ = require('lodash');
 const path = require('path');
+
 const webpack = require('webpack');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
-const HasteMap = require('jest-haste-map');
-const _ = require('lodash');
-
-
-const rootDir = path.resolve(__dirname, '../..');
-const hasteMap = new HasteMap({
-    "extensions": [
-      "snap",
-      "js",
-      "json",
-      "jsx",
-      "node"
-    ],
-    "ignorePattern": /SOME_COMPLEX_UNLIKELY_TO_HAPPEN_PATTERN/,
-    "maxWorkers": 7,
-    "name": rootDir.replace(/[\/\\]/g, '_'),
-    "platforms": [
-      "ios",
-      "android"
-    ],
-    "providesModuleNodeModules": [],
-    "resetCache": false,
-    "retainAllFiles": false,
-    "roots": [
-      rootDir,
-    ],
-    "useWatchman": true,
-}).build();
-
-const ProvidesModuleResolver = {
-    apply: function(resolver) {
-        resolver.plugin('module', function(request, callback) {
-            hasteMap.then((hasteMap) => {
-                const module = hasteMap.moduleMap.getModule(request.request);
-                if (module) {
-                    callback(null, {
-                        path: module,
-                        query: request.query,
-                        file: true,
-                        resolved: true
-                    });
-                } else {
-                    callback();
-                }
-            });
-        });
-    }
-};
+const HasteMapWebpackResolver = require('haste-map-webpack-resolver');
 
 module.exports = {
     context: path.resolve(__dirname, '..'),
@@ -76,7 +31,9 @@ module.exports = {
         ],
     },
     resolve: {
-        plugins: [ProvidesModuleResolver],
+        plugins: [new HasteMapWebpackResolver({
+            rootPath: path.resolve(__dirname, '../..'),
+        })],
     },
     plugins: [
         new webpack.BannerPlugin(
